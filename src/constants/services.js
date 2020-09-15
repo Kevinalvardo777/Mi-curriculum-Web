06cc77b465e492e17c784487d4b700b130458e34
kevin <kevinalvapearson777@gmail.com>
2020-09-14 21:20:01 -0500

    curriculum web inicios, seccion navbar, Hero seccion, services

---
+++ src/constants/services.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    icon: <FaCode className="service-icon" />,
+    title: "web development",
+    text: `I like to develop web apps with the best programming practices and with a great performance, i manage top technolagy in web market today`,
+  },
+  {
+    id: 2,
+    icon: <FaSketch className="service-icon" />,
+    title: "Mobile development",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+  {
+    id: 3,
+    icon: <FaAndroid className="service-icon" />,
+    title: "app design",
+    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+  },
+]
